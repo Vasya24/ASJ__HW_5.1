be5c72d2ec9d666a9025a66c2f7e27685f01ee9e
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-13 14:58:03 +0300

    Test modified

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,7 +3,10 @@ import { sorting, orderByProps, keysArr, otherStuff, mainArr } from '../sorting'
 
 
 test('Checking', () => {
-
+    orderByProps;
+    keysArr;
+    otherStuff;
+    mainArr;
     sorting()
     expect(obj.attack).toBe(80)
 })
